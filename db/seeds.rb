562684ad2c851db34bf0b73165d53eb257ddd6dc
fedetaglia <fede.tagliabue@gmail.com>
2014-04-12 12:23:28 +1000

    seeds user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+User.create(username: 'Andrew', email:'andrew@andrew.com', password:'password', password_confirmation:'password')
+User.create(username: 'Luke', email:'luke@luke.com', password:'password', password_confirmation:'password')
+User.create(username: 'Alberto', email:'alberto@alberto.com', password:'password', password_confirmation:'password')
+User.create(username: 'Serena', email:'serena@serena.com', password:'password', password_confirmation:'password')
+User.create(username: 'Paul', email:'paul@paul.com', password:'password', password_confirmation:'password')
+User.create(username: 'Nick', email:'nick@nick.com', password:'password', password_confirmation:'password')
+User.create(username: 'Isabel', email:'isabel@isabel.com', password:'password', password_confirmation:'password')
+User.create(username: 'Riccardo', email:'riccardo@riccardo.com', password:'password', password_confirmation:'password')
+User.create(username: 'Lalla', email:'lalla@lalla.com', password:'password', password_confirmation:'password')
+User.create(username: 'Emily', email:'emily@emily.com', password:'password', password_confirmation:'password')
+User.create(username: 'sam', email:'sam@sam.com', password:'password', password_confirmation:'password')
+User.create(username: 'Pranava', email:'pranava@pranava.com', password:'password', password_confirmation:'password')
+User.create(username: 'Andrea', email:'andrea@andrea.com', password:'password', password_confirmation:'password')
